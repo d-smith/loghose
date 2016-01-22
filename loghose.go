8864ce5093176ef01f1f8ea2afe96519392056b4
d-smith <doug.smith.mail@gmail.com>
2016-01-22 10:22:19 -0700

    Made stream put async, added verbose command line option

--- loghose.go
+++ loghose.go
@@ -12,6 +12,7 @@ import (
 	"strconv"
 )
 
+var quiet = true
 
 type streamContext struct {
 	Region string
@@ -19,9 +20,9 @@ type streamContext struct {
 }
 
 
-func fileNameFromArgs() (string, *streamContext, error) {
-	if len(os.Args) != 3 {
-		return "",nil, errors.New("Usage: loghost <stream name> <file to tail>")
+func fileNameFromArgs() (string, *streamContext, bool, error) {
+	if len(os.Args) < 4 {
+		return "",nil, false, errors.New("Usage: loghost <stream name> <file to tail> verbose|quiet")
 	}
 
 	defaultRegion := "us-east-1"
@@ -36,7 +37,7 @@ func fileNameFromArgs() (string, *streamContext, error) {
 		StreamName: os.Args[1],
 	}
 
-	return os.Args[2], sc, nil
+	return os.Args[2], sc, os.Args[3] == "verbose", nil
 }
 
 
@@ -49,32 +50,42 @@ func writeLineToFirehose(svc *kinesis.Kinesis, streamName string, line string) {
 		PartitionKey:              aws.String(partitionKey),
 		StreamName:                aws.String(streamName),
 	}
-	resp, err := svc.PutRecord(params)
+	_, err := svc.PutRecord(params)
 
 	if err != nil {
 		// Print the error, cast err to awserr.Error to get the Code and
 		// Message from an error.
-		fmt.Println(err.Error())
+		fmt.Println("error: " + err.Error())
 		return
 	}
 
 	// Pretty-print the response data.
-	fmt.Println(resp)
+	if quiet == false {
+		fmt.Println("data sent")
+	}
 }
 
 
 func main() {
-	fileToTail, sc, err := fileNameFromArgs()
+	fileToTail, sc, verbose, err := fileNameFromArgs()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
 		os.Exit(1)
+	}
+
+	if verbose {
+		quiet = false
 	}
 
 	svc := kinesis.New(session.New(),&aws.Config{Region: aws.String(sc.Region)})
 
 	t, err := tail.TailFile(fileToTail, tail.Config{Follow: true})
 	for line := range t.Lines {
-		//fmt.Println(line.Text)
-		writeLineToFirehose(svc, sc.StreamName, line.Text)
+		go func (line string) {
+			if quiet == false {
+				fmt.Println("send: ", line)
+			}
+			writeLineToFirehose(svc, sc.StreamName, line)
+		}(line.Text)
 	}
 }
